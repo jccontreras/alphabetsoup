20f6e0b30b1cd49ef979524506a209254e29c90e
Ragnarok <juankcontreras00@gmail.com>
2021-05-05 22:49:56 -0500

    update commit

--- src/components/Matrix.vue
+++ src/components/Matrix.vue
@@ -2,29 +2,56 @@
   <div class="container">
     <div class="row" v-for="(wordh) in arraywords" v-bind:key="wordh"
          style="background-color: aquamarine">
-      <div class="col-sm" v-for="(wordv, indexv) in arraywords" v-bind:key="wordv"
-           style="border-color: #2c3e50;">
-        {{ indexv }}
+      <div class="col-sm" v-for="(wordv) in wordh" v-bind:key="wordv">
+        {{ wordv }}
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { WORDS } from '@/store/constants';
+import * as constant from '@/store/constants';
 
 export default {
   name: 'Matrix',
   data() {
     return {
-      arraywords: [],
+      arraywords: {
+        type: Array,
+      },
     };
   },
   mounted() {
-    this.arraywords = WORDS.split('/');
+    this.buildSoup();
   },
   methods: {
-    insertWordsList() {
+    buildSoup() {
+      this.arraywords = [];
+      let index = 0;
+      while (index < 15) {
+        let index2 = 0;
+        this.arraywords[index] = [];
+        while (index2 < 15) {
+          // eslint-disable-next-line max-len
+          const result = constant.ALPHABET.charAt(Math.floor(Math.random() * constant.ALPHABET.length));
+          this.arraywords[index][index2] = result;
+          index2 += 1;
+        }
+        index += 1;
+      }
+      const array = constant.WORDS.split('/');
+      this.insertWordsList(array[1]);
+    },
+    insertWordsList(word) {
+      const wordarray = word.split('');
+      let index = 0;
+      // eslint-disable-next-line prefer-destructuring
+      // alert(wordarray[0]);
+      this.list[4][4] = 'Ñ';
+      while (index < wordarray.length) {
+        // alert(wordarray[index]);
+        index += 1;
+      }
     },
   },
 };
